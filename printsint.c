18359138b1e61b7c7e54e6a9532c60ea7e889238
Faith <faykimanthi@gmail.com>
2023-11-14 11:22:14 -0800

    printsint.c

--- printsint.c
+++ printsint.c
@@ -12,40 +12,13 @@ int print_i(int arg)
 	if (arg < 0)
 	{
 		_putchar('-');
-<<<<<<< HEAD
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	_putchar(last + '0');
-	}
-	return (i);
-=======
 		count++;
 		arg *= -1;
 		k = arg;
 	}
 	k /= 10;
 	if (k)
-		count += print_i(k);
+		count += numprint(k);
 	count += _putchar(((unsigned int) arg % 10) + 48);
 	return (count);
->>>>>>> 953eaa71b7d14d74185300a646e2084fd6fb0813
 }
