953eaa71b7d14d74185300a646e2084fd6fb0813
AyiembaPearl <dorothyayiemba@gmail.com>
2023-11-14 06:17:47 -0800

    add number funcs

---
+++ printsint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_i - a function that prints a number using recursion
+ * @arg: the number to be printed
+ * Return: number of digits printed
+ */
+int print_i(int arg)
+{
+	unsigned int k = arg;
+	int count = 0;
+
+	if (arg < 0)
+	{
+		_putchar('-');
+		count++;
+		arg *= -1;
+		k = arg;
+	}
+	k /= 10;
+	if (k)
+		count += print_i(k);
+	count += _putchar(((unsigned int) arg % 10) + 48);
+	return (count);
+}
